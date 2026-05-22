cf8c4d8eb3af18257c96e95ec0c44e8f5130a16e
Artemy <dorneusxo@gmail.com>
2026-05-22 18:05:22 +0300

    first

---
+++ main.js
@@ -0,0 +1,58 @@
+console.log(343);
+
+const axios = require('axios');
+const fs = require('fs');
+const { YtDlp } = require('ytdlp-nodejs');
+const ytdlp = new YtDlp();
+const { createWriteStream } = require('fs');
+const express = require('express')
+const app = express();
+const port = 3000;
+const { YTHandler } = require('../bot/src/Handler.js')
+const yth = new YTHandler();
+require('dotenv').config();
+
+
+const youtube = require('googleapis').google.youtube({
+    version: 'v3',
+    auth: process.env.API_KEY,
+});
+
+app.get('/', (req, res)=>{
+  res.send("<h1 style='color: red'>Пошел нахуй</h1>")
+})
+
+app.get('/download', (req, res)=>{
+  yth.download('https://youtu.be/fdvof0iRJf8?si=JghsviFD6bMsUcP3')
+  .then(resp=>{
+    console.log(resp)
+    res.send("<h1 style='color: red'>Пошел нахуй download</h1>")
+  })
+})
+
+app.get('/pipeload', async (req, res)=>{
+  yth.pipeload('https://youtu.be/fdvof0iRJf8?si=JghsviFD6bMsUcP3')
+  .then(resp=>{
+    console.log(resp)
+    res.send("<h1 style='color: red'>Пошел нахуй pipeload</h1>")
+  })
+})
+
+app.get('/list', (req, res)=>{
+  yth.getList("Жмиль", 3, "channel").then(data=>console.log(data) || res.json(data))
+})
+
+app.get('/info', (req, res)=>{
+  yth.getVideoInfo('r6JyVyUxgBs').then(data=>console.log(data) || res.json(data))
+})
+app.get('/channelInfo', (req, res)=>{
+  yth.getChannelInfo('UCmSImlPUez2i1wvsctTB0Gw').then(data=>console.log(data) || res.json(data))
+})
+
+app.get('/channelVideo', (req, res)=>{
+  yth.getChannelVideos('UCmSImlPUez2i1wvsctTB0Gw', 5).then(data=>console.log(data) || res.json(data))
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
---
+++ src/Handler.js
@@ -0,0 +1,86 @@
+const axios = require('axios');
+const fs = require('fs');
+const { YtDlp } = require('ytdlp-nodejs');
+const ytdlp = new YtDlp();
+const { createWriteStream } = require('fs');
+require('dotenv').config();
+const youtube = require('googleapis').google.youtube({
+    version: 'v3',
+    auth: process.env.API_KEY,
+});
+
+class YTHandler{
+    async download(url){
+        const result = await ytdlp
+        .download(url)
+        .filter('mergevideo')
+        .quality('1080p')
+        .type('mp4')
+        .output('/home/artem/Projects/vk_bot/bot/src/videos')
+        .on('progress', (p) => console.log(`${p.percentage_str}`))
+/*         .on('finish', ()=>{console.log(343434)})
+        .on('error', e=>{console.log(e.message)}) */
+        .run();
+        return result;
+    }
+    async pipeload(url){
+        const result = await ytdlp
+        .stream(url)
+        .filter('audioandvideo')
+        .quality('1080p')
+        .type('mp4')
+        .on('progress', (p) => console.log(`${p.percentage_str}`))
+        .pipeAsync(createWriteStream('video.mp4'));
+        return result;
+    }
+    async getList(query, maxResults=10, type="video"){
+/*         const request = `GET https://www.googleapis.com/youtube/v3/search?`+
+        `part=snippet` +
+        `&q=${encodeURIComponent(query)}` +
+        `&maxResults=${maxResults}` +
+        `&type=${type}` +
+        `&key=${process.env.API_KEY}`;         */
+        const request = `https://www.googleapis.com/youtube/v3/search`
+        const params = {
+            part: 'snippet',
+            q: query,
+            maxResults: maxResults,
+            type: type,
+        };
+
+        try{
+            //const responce = await axios.get(request, {params: params});
+            const responce = await youtube.search.list(params)
+            return responce.data;
+        } catch(e){
+            console.log("Вот такая ошибка: " + e.message)
+            console.log("Вот такой ответ: " + e.responce?.data)
+        }
+    }
+    async getVideoInfo(id){
+        const responce = await youtube.videos.list({
+            part: ['snippet'],
+            id: id,
+        })
+        return responce.data.items[0];
+    }
+    async getChannelInfo(id){
+        const responce = await youtube.channels.list({
+            part: ['snippet'],
+            id: id,
+        })
+        return responce.data.items[0];
+    }
+    async getChannelVideos(id, maxResults=10, order="date"){
+        const responce = await youtube.search.list({
+            channelId: /* "UCmSImlPUez2i1wvsctTB0Gw" */id,
+            part: "snippet",
+            maxResults: maxResults,
+            type: "video",
+            order: order,
+        })
+        return responce.data;
+    }
+}
+
+module.exports = { YTHandler };
